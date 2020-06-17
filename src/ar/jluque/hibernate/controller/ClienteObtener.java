ef8ba3cec1641aeb96e2e24c524b311fc64b803a
Julio Luque <julio.a.luque@gmail.com>
2020-06-17 01:22:50 -0300

    Excepciones y cascada Delete

--- src/ar/jluque/hibernate/controller/ClienteObtener.java
+++ src/ar/jluque/hibernate/controller/ClienteObtener.java
@@ -22,11 +22,17 @@ public class ClienteObtener {
 			DetallesCliente dc = ss.get(DetallesCliente.class, 2);
 			System.out.println(dc);
 			System.out.println(dc.getCliente());
+			
+			System.out.println("Eliminar en cascada: ");
+			ss.delete(dc);
 			ss.getTransaction().commit();
-			ss.close();
+			
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("Mensaje de error: ");
+			e.printStackTrace();
+			
 		} finally {
+			ss.close();
 			sf.close();
 		}
 
